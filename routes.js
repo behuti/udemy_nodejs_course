b531bc39b61a051c9bba2251bd0250221a75baec
behuti <echenao@gmail.com>
2024-06-18 12:41:12 -0500

    write message on create-user action

--- routes.js
+++ routes.js
@@ -3,38 +3,43 @@ const fs = require("fs");
 const requestHandler = (req, res) => {
   const url = req.url;
   const method = req.method;
+
+  // Home path
   if (url === "/") {
     res.write("<html>");
-    res.write("<head><title>Enter Message</title></head>");
+    res.write("<head><title>My App</title></head>");
+    res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
+    res.write("</html>");
+    return res.end();
+  }
+
+  if (url === "/users" && method === "GET") {
+    res.write("<html>");
+    res.write("<head><title>My App</title></head>");
     res.write(
-      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+      '<body><form action="/create-user" method="POST"><input type="text" name="username"><button type="submit">Send</button></form></body>'
     );
     res.write("</html>");
     return res.end();
   }
 
-  if (url === "/message" && method === "POST") {
+  if (url === "/create-user" && method === "POST") {
     const body = [];
     req.on("data", (chunk) => {
-      console.log(chunk);
       body.push(chunk);
     });
     return req.on("end", () => {
       const parsedBody = Buffer.concat(body).toString();
       const message = parsedBody.split("=")[1];
-      fs.writeFile("message.txt", message, (err) => {
-        res.statusCode = 302;
-        res.setHeader("Location", "/");
-        return res.end();
-      });
+
+      console.log("final message: ", message);
+      // fs.writeFile("message.txt", message, (err) => {
+      //   res.statusCode = 302;
+      //   res.setHeader("Location", "/");
+      //   return res.end();
+      // });
     });
   }
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<head><title>My First Page</title><head>");
-  res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
-  res.write("</html>");
-  res.end();
 };
 
 module.exports = requestHandler;
